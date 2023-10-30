23e322a3c258cf27feccd296e6daad4332199d6c
Vincent Delitz <vincent.delitz@tum.de>
2023-10-30 14:05:42 +0100

    added substack

--- src/lib/sections/PasskeyDemo.svelte
+++ src/lib/sections/PasskeyDemo.svelte
@@ -1,120 +1,120 @@
 <script>
-	import {PUBLIC_CORBADO_PROJECT_ID} from '$env/static/public';
-	import Corbado from '@corbado/webcomponent';
-	import '@corbado/webcomponent/pkg/auth_cui.css';
-	import { Button, Card, Heading } from 'flowbite-svelte';
-	import { onMount } from 'svelte';
+    import {PUBLIC_CORBADO_PROJECT_ID} from '$env/static/public';
+    import Corbado from '@corbado/webcomponent';
+    import '@corbado/webcomponent/pkg/auth_cui.css';
+    import {Button, Card, Heading} from 'flowbite-svelte';
+    import {onMount} from 'svelte';
 
-	export let data;
+    export let data;
 
-	let scrollVisible = true;
-	let username = '';
-	const corbado = new Corbado.Session(PUBLIC_CORBADO_PROJECT_ID);
+    let scrollVisible = true;
+    let username = '';
+    const corbado = new Corbado.Session(PUBLIC_CORBADO_PROJECT_ID);
 
-	const corbadoUser = corbado.refresh((user) => {
-		console.log(user);
-	});
+    const corbadoUser = corbado.refresh((user) => {
+        console.log(user);
+    });
 
-	function gtag_report_conversion(url) {
-		var callback = function () {
-			if (typeof url != 'undefined') {
-				window.location = url;
-			}
-		};
-		gtag('event', 'conversion', {
-			send_to: 'AW-10943248379/zc51CP29ybIYEPvvkuIo',
-			event_callback: callback
-		});
-		return false;
-	}
+    function gtag_report_conversion(url) {
+        var callback = function () {
+            if (typeof url != 'undefined') {
+                window.location = url;
+            }
+        };
+        gtag('event', 'conversion', {
+            send_to: 'AW-10943248379/zc51CP29ybIYEPvvkuIo',
+            event_callback: callback
+        });
+        return false;
+    }
 
-	onMount(() => {
-		username = localStorage.getItem('username') || '';
-		const handleScroll = () => {
-			scrollVisible = window.scrollY < 50;
-		};
+    onMount(() => {
+        username = localStorage.getItem('username') || '';
+        const handleScroll = () => {
+            scrollVisible = window.scrollY < 50;
+        };
 
-		if (data && data.jwt) {
-			gtag_report_conversion();
-		}
+        if (data && data.jwt) {
+            gtag_report_conversion();
+        }
 
-		window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll);
 
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	});
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
 
-	function scrollToContinue() {
-		window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
-	}
+    function scrollToContinue() {
+        window.scrollBy({top: window.innerHeight, behavior: 'smooth'});
+    }
 </script>
 
 <section class="bg-secondary h-screen">
-	<div
-		class="mx-auto max-w-screen-xl flex flex-col text-center items-center justify-center w-full h-full"
-	>
-		<Heading tag="h1" class="mb-8 md:mb-16">Passkeys demo</Heading>
-		<Card class="w-11/12 md:w-full mb-16">
-			{#if data && data.jwt}
-				<Heading tag="h4">That’s it.</Heading>
-				<Heading tag="h4">You’re logged in.​</Heading>
-				<Button href="/api/logout" pill class="bg-primary text-white mt-8 ">Log out</Button>
-			{:else}
-				<corbado-auth
-					style="border: none; padding: 0px"
-					project-id={PUBLIC_CORBADO_PROJECT_ID}
-					conditional="yes"
-					auto_detect_language="no"
-					fallback_language="en"
-					login_title="Try passkey login"
-					login_btn="Passkey login"
-					register_title="Try passkey signup"
-					register_btn="Passkey signup"
-					page={data.signedIn ? 'login' : 'register'}
-				>
-					<input
-						name="username"
-						id="corbado-username"
-						value={username}
-						required
-						autocomplete="webauthn"
-					/>
-				</corbado-auth>
-			{/if}
-		</Card>
-		<button
-			class="invisible md:visible opacity-100 transition-opacity duration-300 absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-70 py-2 px-4 rounded-full text-black focus:outline-none"
-			style="opacity: {scrollVisible ? 1 : 0}; transition: opacity 300ms;"
-			on:click={scrollToContinue}
-		>
-			Scroll to continue
-		</button>
-	</div>
+    <div
+            class="mx-auto max-w-screen-xl flex flex-col text-center items-center justify-center w-full h-full"
+    >
+        <Heading tag="h1" class="mb-8 md:mb-16">Passkeys demo</Heading>
+        <Card class="w-11/12 md:w-full mb-16">
+            {#if data && data.jwt}
+                <Heading tag="h4">That’s it.</Heading>
+                <Heading tag="h4">You’re logged in.​</Heading>
+                <Button href="/api/logout" pill class="bg-primary text-white mt-8 ">Log out</Button>
+            {:else}
+                <corbado-auth
+                        style="border: none; padding: 0px"
+                        project-id={PUBLIC_CORBADO_PROJECT_ID}
+                        conditional="yes"
+                        auto_detect_language="no"
+                        fallback_language="en"
+                        login_title="Try passkey login"
+                        login_btn="Passkey login"
+                        register_title="Try passkey signup"
+                        register_btn="Passkey signup"
+                        page={data.signedIn ? 'login' : 'register'}
+                >
+                    <input
+                            name="username"
+                            id="corbado-username"
+                            value={username}
+                            required
+                            autocomplete="webauthn"
+                    />
+                </corbado-auth>
+            {/if}
+        </Card>
+        <button
+                class="invisible md:visible opacity-100 transition-opacity duration-300 absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-70 py-2 px-4 rounded-full text-black focus:outline-none"
+                style="opacity: {scrollVisible ? 1 : 0}; transition: opacity 300ms;"
+                on:click={scrollToContinue}
+        >
+            Scroll to continue
+        </button>
+    </div>
 </section>
 
 <style>
-	corbado-auth {
-		--primary-color: #1953ff;
-		--primary-color-rgb: 25, 83, 255;
-		--primary-background-color: transparent;
-		--primary-hover-color: #1145df;
+    corbado-auth {
+        --primary-color: #1953ff;
+        --primary-color-rgb: 25, 83, 255;
+        --primary-background-color: transparent;
+        --primary-hover-color: #1145df;
 
-		--secondary-font-color: #59acff;
-		--secondary-background-color: transparent;
-		--secondary-border-color: #59acff;
+        --secondary-font-color: #59acff;
+        --secondary-background-color: transparent;
+        --secondary-border-color: #59acff;
 
-		--secondary-background-hover-color: transparent;
-		--secondary-border-hover-color: #59acff;
-		--secondary-font-hover-color: #59acff;
-		--heading-color: #000;
-		--text-color: #000;
-		--text-disabled-color: #999;
-		--light-color: #59acff;
-		--error-color: #ff4c51;
-		--primary-font: 'Space Grotesk', sans-serif;
-		--secondary-font: 'Inter', sans-serif;
-		--border-color: rgba(143, 155, 191, 0.5);
-		--email-provider-btn-color: rgba(143, 155, 191, 0.5);
-	}
+        --secondary-background-hover-color: transparent;
+        --secondary-border-hover-color: #59acff;
+        --secondary-font-hover-color: #59acff;
+        --heading-color: #000;
+        --text-color: #000;
+        --text-disabled-color: #999;
+        --light-color: #59acff;
+        --error-color: #ff4c51;
+        --primary-font: 'Space Grotesk', sans-serif;
+        --secondary-font: 'Inter', sans-serif;
+        --border-color: rgba(143, 155, 191, 0.5);
+        --email-provider-btn-color: rgba(143, 155, 191, 0.5);
+    }
 </style>
