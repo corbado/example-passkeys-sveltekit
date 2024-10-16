d1b76040c914569b115ce2a646890e8febf0b977
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2024-10-16 15:07:13 +0200

    Adapted to new session management

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,33 +1,25 @@
 import { type RequestEvent } from '@sveltejs/kit';
 import { PUBLIC_CORBADO_PROJECT_ID } from '$env/static/public';
 import { CORBADO_API_SECRET } from '$env/static/private';
+import { CORBADO_FRONTEND_API } from '$env/static/private';
+import { CORBADO_BACKEND_API } from '$env/static/private';
 import { SDK, Config } from '@corbado/node-sdk';
 
-const config = new Config(PUBLIC_CORBADO_PROJECT_ID, CORBADO_API_SECRET);
+const config = new Config(PUBLIC_CORBADO_PROJECT_ID, CORBADO_API_SECRET, CORBADO_FRONTEND_API, CORBADO_BACKEND_API);
 const sdk = new SDK(config);
 
 export async function load({ cookies }: RequestEvent) {
-    const cbo_short_session = cookies.get("cbo_short_session");
-    if (!cbo_short_session) {
+    const sessionToken = cookies.get("cbo_session_token");
+    if (!sessionToken) {
         return {user: undefined}
     }
-    try {
-        const user = await sdk.sessions().getCurrentUser(cbo_short_session);
-        if (!user.isAuthenticated()) {
-            return {user: undefined}
-        }
-        return { user: { email: user.getEmail(), userID: user.getID() } };
-    } catch {
-        // session cookie was invalid
-        return {user: undefined}
-    }
-}
 
-function parseCookies(cookieHeader: string): Record<string, string> {
-    return Object.fromEntries(
-        cookieHeader.split(';').map((cookie) => {
-            const [name, ...rest] = cookie.trim().split('=');
-            return [name, rest.join('=')];
-        })
-    );
+    try {
+        const user = await sdk.sessions().validateToken(sessionToken);
+
+        return { user: { userId: user.userId, fullName: user.fullName } };
+    } catch {
+        // session token was invalid
+        return {user: undefined}
+    }
 }
